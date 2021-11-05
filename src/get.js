ece2de1dd672432b1029c8fbc90f21a58c5db31e
Wouter <wintvelt@me.com>
2021-11-05 23:27:42 +0100

    run query to get dependencies

--- src/get.js
+++ src/get.js
@@ -3,15 +3,30 @@ import middy from '@middy/core'
 import errorLogger from '@middy/error-logger'
 import httpErrorHandler from '@middy/http-error-handler'
 import cors from '@middy/http-cors'
-// import { dynamo } from "./libs/dynamo-lib";
+import { dynamo } from "./libs/dynamo-lib";
 
 
 const baseHandler = async (event) => {
-    const encodedId = event.queryStringParameters.id
-    const dependency = decodeURIComponent(encodedId)
+    const id = event.queryStringParameters.id // decoding already done
 
-    const response = { result: 'success', message: `parsed as "${dependency}" with encoded "${encodedId}""` }
-    return { statusCode: 200, body: JSON.stringify(response) }
+    const queryParams = {
+        TableName: process.env.TABLE_NAME,
+        IndexName: "dependencyIndex",
+        KeyConditionExpression: "dependency = :dep",
+        ExpressionAttributeValues: {
+            ":dep": id
+        },
+    };
+
+    let result
+    try {
+        result = await dynamo.query(queryParams)
+    } catch (error) {
+        console.error(error.message);
+        throw new Error(error.message);
+    }
+
+    return { statusCode: 200, body: JSON.stringify(result.Items) }
 }
 
 export const handler = middy(baseHandler)
