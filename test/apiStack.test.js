dfcf01e7e945cd0f4acc721e0cd28d4e4cbb86de
Wouter <wintvelt@me.com>
2021-11-09 22:36:16 +0100

    add missing import

--- test/apiStack.test.js
+++ test/apiStack.test.js
@@ -2,6 +2,7 @@ import { expect, haveResource } from "@aws-cdk/assert";
 import * as sst from "@serverless-stack/resources";
 import ApiStack from "../stacks/apiStack";
 import DbStack from "../stacks/DbStack";
+import queueStack from "../stacks/queueStack";
 
 test("Test Stack", () => {
   const app = new sst.App();
