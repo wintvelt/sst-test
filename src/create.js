440f96b0fdbef947bc447be167aad5a0060acd8b
Wouter <wintvelt@me.com>
2021-11-17 23:40:19 +0100

    try with middy middleware

--- src/create.js
+++ src/create.js
@@ -5,7 +5,8 @@ import errorLogger from '@middy/error-logger'
 import validator from '@middy/validator'
 import httpErrorHandler from '@middy/http-error-handler'
 import cors from '@middy/http-cors'
-import Sentry from "@sentry/serverless"
+// import Sentry from "@sentry/serverless"
+import sentryErrorHandler from '@joblocal/middy-sentry-error-handler';
 
 import { dynamo } from "./libs/dynamo-lib"
 import { inputSchema } from './libs/create-input-schema'
@@ -95,11 +96,13 @@ const baseHandler = async (event) => {
     return { statusCode: 200, body: JSON.stringify(response) }
 }
 
-export const handler = Sentry.AWSLambda.wrapHandler(
-    middy(baseHandler)
-        .use(errorLogger())
-        .use(jsonBodyParser()) // parses the request body when it's a JSON and converts it to an object
-        .use(validator({ inputSchema })) // validates the input
-        .use(cors())
-        .use(httpErrorHandler({ fallbackMessage: 'server error' }))
-)
+// export const handler = Sentry.AWSLambda.wrapHandler(
+export const handler = middy(baseHandler)
+    .use(sentryErrorHandler({
+        dsn: process.env.SENTRY_DSN,
+    }))
+    .use(errorLogger())
+    .use(jsonBodyParser()) // parses the request body when it's a JSON and converts it to an object
+    .use(validator({ inputSchema })) // validates the input
+    .use(cors())
+    .use(httpErrorHandler({ fallbackMessage: 'server error' }))
