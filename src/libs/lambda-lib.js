c19d815a1c99f96b301d551fa495cc6cd28e752f
Wouter <wintvelt@me.com>
2021-12-01 17:02:04 +0100

    wrap lambda

--- src/libs/lambda-lib.js
+++ src/libs/lambda-lib.js
@@ -1,6 +1,9 @@
+import AWSXRay from "aws-xray-sdk-core"
 import AWS from "aws-sdk"
 
-const lambdaFunc = new AWS.Lambda()
+const AWSWrapped = AWSXRay.captureAWS(AWS)
+
+const lambdaFunc = new AWSWrapped.Lambda()
 
 export const lambda = {
     invoke: (params) => lambdaFunc.invoke(params).promise()
